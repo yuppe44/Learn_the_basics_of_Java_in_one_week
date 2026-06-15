ed8f15d20ac9a1c4e6103bb5304ef99f39b7d9bf
YUHEI FUJITA <kame3mikan3@gmail.com>
2026-06-15 19:38:19 +0900

    feat: Sample101.javaを作成

---
+++ src/day1/Sample101.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Sample101 {
+    /*
+     * Javaを使った最も基本的なプログラム
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello,World."); // 「Hello,World.」という文字列を表示します。
+    }
+}
